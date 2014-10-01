98e31ddad1c2d2d99d80637d15d8b8df23074656
GutsOut <r.yaroma@normativka.by>
2014-10-01 14:44:03 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+<ul>
+	<li><a href="phpinfo.php"><?php echo 'PHP '.phpversion();?></a></li>
+	<li><?php echo 'Apache '.apache_get_version();?></li>
+</ul>
+	<?php
+	?>
+</body>
+</html>
