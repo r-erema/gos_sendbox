0fad9d8296d9838ee64085fe142d8209524acff3
r.yaroma <r.yaroma@normativka.by>
2014-12-29 12:39:12 +0300

    digest

--- pg/coder/index.php
+++ pg/coder/index.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<title>coder</title>
 		<script src="js/jquery-2.1.3.min.js"></script>
-		<script src="js/newjs.js"></script>
+		<script src="js/js.js"></script>
 		<link rel="stylesheet" type="text/css" href="css/css.css">
 	</head>
 	<body>
--- pg/coder/js/js.js
+++ pg/coder/js/js.js
@@ -1,110 +1,115 @@
 $(function () {
-	var config ={
-		'profiz' : {
-			mags : [
-				{
-					'code' : 'kr',
-					'abbr' :'КР'
-				},
-				{
-					'code' : 'sr',
-					'abbr' :'СР'
-				},
-				{
-					'code' : 'se',
-					'abbr' :'СЭ'
-				},
-				{
-					'code' : 'sec',
-					'abbr' :'СЭК'
-				},
-				{
-					'code' : 'peo',
-					'abbr' :'ПЭО'
-				},
-				{
-					'code' : 'eco',
-					'abbr' :'ЭКО'
-				}
-			],
-			addresseeForums : [
-				{
-					'name' : 'buhgalter-info.ru',
-					'link' : 'http://buhgalter-info.ru/'
-				},
-				{
-					'name' : 'kadrovik-info.ru',
-					'link' : 'http://kadrovik-info.ru/'
-				},
-				{
-					'name' : 'economist-info.ru',
-					'link' : 'http://economist-info.ru/'
-				},
-				{
-					'name' : 'sekretar-info.ru',
-					'link' : 'http://sekretar-info.ru/'
-				},
-				{
-					'name' : 'ecolog-info.ru',
-					'link' : 'http://ecolog-info.ru/'
-				}
-			]
-		}
+
+	var config = {
+		'profizMags' : [
+			{'code':'kr'},
+			{'code':'sr'},
+			{'code':'se'},
+			{'code':'eco'},
+			{'code':'sec'},
+			{'code':'peo'}
+		],
+		'addresseeForums' : [
+			{'name' : 'buhgalter-info.ru'},
+			{'name' : 'kadrovik-info.ru'},
+			{'name' : 'economist-info.ru'},
+			{'name' : 'sekretar-info.ru'},
+			{'name' : 'ecolog-info.ru'}
+		]
 	};
 
-	$('#parser-select').on('change', function() {
-		$('#profiz-mags-to-parse-buttons-panel').remove();
-		$('#parse-form').empty().attr('name', $(this).val());
-		switch($(this).val()) {
-			case 'profizDigestParser' :
-				$('#parse-form').append('<input type="hidden" value="' + $(this).val() + '" name="parserName">');
-				$('#parse-form').addAdreseeForumButtons();
-				$.each($(this).addProfizButtonsPanel().children(), function() {
-					$(this).on('click', function () {
-						$(this).attr('disabled', true);
-						if($('#parse-form input[type=submit]').length == 0) {
-							$('#parse-form').append('<input type="submit">');
-						}
-						$('#parse-form').addTextarea($(this).data('mag'));
-						return false;
-					})
-				});
-				break;
-			default: $('#inf-message').text('Неизвестный select');
+	var body = $('body');
+	var parserSelect = $('[name="parser-select"]');
+	var controlPanel = $('#parser-control-panel');
+	var parseForm = $('#parse-form');
+	var formWrapper = $('#form-wrapper');
+	var submitBtn = parseForm.find('input[type="submit"]');
+	var parserName = parseForm.find('input[name="parserName"]');
+
+	parserSelect.on('change', function() {
+		controlPanel.hide('fast').empty();
+		formWrapper.hide('fast');
+		parseForm.children().not(submitBtn).not(parserName).remove();
+		var selectValue = $(this).val();
+		parserName.attr('value',selectValue);
+		switch(selectValue) {
+			case 'profizDigestParser' : controlPanel.apppendProfizMagsDigestControls().show('fast'); break;
+			default : body.prependMessage('error','Неизвестный парсер'); break;
 		}
 	});
 
-	$.prototype.addAdreseeForumButtons = function() {
-		var div = $('<div id="addressee-forums-buttons">Форум получателей:<br /></div>');
-		$.each(config.profiz.addresseeForums, function () {
-			div.append('<input type="radio" name="addresseeForum" value="' + this.name + '">' + this.name + '<br />');
-		});
-		$(this).append(div);
-		return div;
+
+	/**
+	 * Вставляет блок с информационным сообщением в начало this
+	 * @param type
+	 * @param text
+	 * @returns {*|jQuery|HTMLElement}
+	 */
+	$.prototype.prependMessage = function(type, text) {
+		$('.message').remove();
+		var messageBlock = $('<div class="message">' + text + '</div>');
+		messageBlock.append(createRemover(messageBlock));
+		return messageBlock.prependTo($(this)).addClass(type).show('fast');
 	};
 
-	$.prototype.addProfizButtonsPanel = function() {
-		var panel = $('<div id="profiz-mags-to-parse-buttons-panel"></div>');
-		$.each(config.profiz.mags, function() {
-			panel.append('<button id="add-' + this.code + '-textarea" data-mag="' + this.code + '">'+ this.abbr + '</button>');
+	/**
+	 *
+	 * @returns {$}
+	 */
+	$.prototype.apppendProfizMagsDigestControls = function () {
+		$(this).append(createProfizMagsButtonsPanel());
+		parseForm.prepend(createAddresseeForumsPanel());
+		formWrapper.show();
+		return this;
+	};
+
+	/**
+	 *
+	 * @returns {*|jQuery|HTMLElement}
+	 */
+	var createProfizMagsButtonsPanel = function () {
+		var panel = $('<div id="profiz-mags-to-parse-buttons-panel"><h3>Выберите журналы которые нужно сверстать в один документ</h3></div>');
+		$.each(config.profizMags, function() {
+			$('<button name="add-textarea" data-mag="' + this.code + '">'+ this.code + '</button>').appendTo(panel);
 		});
-		$(this).after(panel);
 		return panel;
 	};
 
-	$.prototype.addTextarea = function (id) {
-		var textareaWrapper = $('<fieldset id="' + id + '-textarea-fieldset"></fieldset>');
-		var removeTextareaLink = $('<a href="#" id="' + id + '-textarea-remover">Удалить</a>');
-		removeTextareaLink.on('click', function () {
-			$('#' + id + '-textarea-fieldset').remove();
-			$('button#add-' + id + '-textarea').attr('disabled',false);
-			if($('#parse-form').children().not('input[type=submit]').length == 1) {
-				$('#parse-form input[type=submit]').remove();
-			}
+	var createAddresseeForumsPanel = function () {
+		var wrapper = $('<div class="addr-forums-wrapper"><h3>Форум на который пойдет рассылка</h3></div>');
+		var list = $('<ul></ul>').appendTo(wrapper);
+		$.each(config.addresseeForums, function () {
+			list.append($('<li><input type="radio" name="addresseeForum" id = ' + this.name + ' value="' + this.name + '"><label for="' + this.name + '">' + this.name + '</label></li>'));
 		});
-		var textarea = $('<textarea id="' + id + '-parse-textarea" cols="70" rows="30" name=texts[' + id + ']>');
-		textareaWrapper.append('<legend>' + id + '</legend>').append(textarea).append(removeTextareaLink);
-		return textareaWrapper.appendTo($(this));
-	}
+		return wrapper;
+	};
+
+	$(document).on('click', '[name="add-textarea"]',function () {
+		var btn = $(this);
+		btn.attr('disabled', 'disabled');
+		var mag = $(this).data('mag');
+		var textAreaWrapper = $('<div class="textarea-wrapper"><h3>' +mag + '</h3></div>');
+		createRemover(textAreaWrapper).appendTo(textAreaWrapper).on('click', function () {
+			btn.removeAttr('disabled');
+		});
+		textAreaWrapper.append();
+		var textarea = $('<textarea id="' + mag + '-parse-textarea" cols="120" rows="20" name=texts[' + mag + ']>');
+		submitBtn.before(textAreaWrapper.append(textarea));
+	});
+
+	/**
+	 *
+	 * @param toRemove
+	 * @returns {*|jQuery|HTMLElement}
+	 */
+	var createRemover = function (toRemove) {
+		var x = $('<a href="#">✖</a>');
+		x.on('click', function () {
+			toRemove.hide('fast', function () {
+				$(this).remove();
+			});
+		});
+		return x;
+	};
 
 });
