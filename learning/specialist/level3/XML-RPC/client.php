e5267d46cd044f26927c485c2662bae0a40a8f8b
r.yaroma <r.yaroma@normativka.by>
2015-01-09 11:38:51 +0300

    coder fixes

--- learning/specialist/level3/XML-RPC/client.php
+++ learning/specialist/level3/XML-RPC/client.php
@@ -8,16 +8,16 @@
 				'method' => 'POST',
 				'header' => "User-Agent: PHPRPC/1.0\r\n" .
 					"Content-Type: text/xml\r\n" .
-					'Content-length: ' . strlen($request_xml) . '\r\n',
-				'content' => $request_xml
+					'Content-length: ' . strlen($request_xml) . "\r\n",
+				'content' => "$request_xml"
 			]
 		];
 		$context = stream_context_create($opts);
 		$retVal = file_get_contents('http://localhost/learning/specialist/level3/XML-RPC/server.php', false, $context);
-		$data = xmlrpc_decode($retVal, 'utf-8');
+		$data = xmlrpc_decode($retVal);
 		var_dump($data);
 	}
 
-	$num = "10";
-	$requestXML = xmlrpc_encode_request('getStock', [$userId, $num], ["encoding" => "utf-8", 'escaping'=>'markup']);
-	make_request(1,$requestXML, 1);
+	$num = "2";
+	$requestXML = xmlrpc_encode_request('getStock', [$userId, $num]);
+	make_request($requestXML, $arMessage, $num);
