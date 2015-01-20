c641c4d20777d6694097d59c7776948a1c3aec1d
r.yaroma <r.yaroma@normativka.by>
2015-01-20 17:48:39 +0300

    learning

--- learning/specialist/level4/PDO/news/news.php
+++ learning/specialist/level4/PDO/news/news.php
@@ -1,6 +1,27 @@
 <?php
 	require_once 'NewsPDO.php';
-
+	$db = new PDO("sqlite:news.db");
+/*
 	$newsDB = new NewsPDO();
 	var_dump($newsDB->saveNews('testTitle', 2, 'testDescr', 'testSource'));
 	var_dump($newsDB->getNews());
+*/
+	class testDescr {
+		public $id;
+		public $title;
+		public $description;
+
+		public function nameToUpper() {
+			return strtoupper($this->title);
+		}
+
+	}
+
+
+$sql = "SELECT description, title, category, id FROM msgs WHERE title = 'testTitle'";
+var_dump($sql);
+$stmt = $db->query($sql);
+//$obj = $stmt->fetch(PDO::FETCH_CLASS|PDO::FETCH_CLASSTYPE);
+//$obj = $stmt->fetchAll(PDO::FETCH_CLASS, 'testDescr');
+//$obj = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+var_dump($obj);
