2123ef19a0ea25da0ff9863c3fca87f8a0debc09
gutsout <eratut@mail.ru>
2015-12-29 21:55:58 +0300

    visitor pattern

---
+++ learning/patterns/patterns/tasks_and_presentation_of_results/Visitor/ArmyVisitor.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace learning\patterns\patterns\tasks_and_presentation_of_results\Visitor;
+
+class ArmyVisitor {
+}
---
+++ learning/patterns/patterns/tasks_and_presentation_of_results/Visitor/TroopCarrierUnit.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Visitor;
+
+class TroopCarrierUnit {
+}
