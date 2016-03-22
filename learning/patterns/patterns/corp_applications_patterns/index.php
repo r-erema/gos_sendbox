da2b828f992ffae5e42c9c98b851112c484ba706
gutsout <r.yaroma@normativka.by>
2016-03-22 18:12:01 +0300

    PageController

--- learning/patterns/patterns/corp_applications_patterns/index.php
+++ learning/patterns/patterns/corp_applications_patterns/index.php
@@ -3,5 +3,9 @@ spl_autoload_register(function ($class) {
     require_once '/home/gutsout/h/gos_sendbox/learning/patterns/patterns/corp_applications_patterns/' . str_replace('\\', '/', $class) . '.php';
 });
 
-\woo\base\ApplicationRegistry::setDSN('DSN-Value');
-$dsn = \woo\base\ApplicationRegistry::getDSN();
+/*\woo\base\ApplicationRegistry::setDSN('DSN-Value');
+$dsn = \woo\base\ApplicationRegistry::getDSN();*/
+
+$controller = new \woo\controller\AddVenueController();
+$controller->process();
+var_dump($controller);
