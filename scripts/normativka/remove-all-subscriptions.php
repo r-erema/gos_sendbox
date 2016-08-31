56c5605838dee839bbe830e6cfa588bdf5713820
gutsout <r.yaroma@corp.profigroup.by>
2016-08-31 09:18:16 +0300

    wqe

--- scripts/normativka/remove-all-subscriptions.php
+++ scripts/normativka/remove-all-subscriptions.php
@@ -12,9 +12,8 @@
 	$userId = $stmt->fetchColumn();
 
 
-	$stmt = $pdo->query("UPDATE nr_codes SET code_is_active = 0 WHERE code_activated_user_id = {$pdo->quote($userId)} AND code_is_active = 1");
-	$stmt->execute();
-	$result[] = "Дективировано кодов: {$stmt->rowCount()}";
+	$count = $pdo->exec("UPDATE nr_codes SET code_is_active = 0 WHERE code_activated_user_id = {$pdo->quote($userId)} AND code_is_active = 1");
+	$result[] = "Дективировано кодов: {$count}";
 
 	$shellCommands = require __DIR__ . '/normativka-cli-init.php';
 	$shellCommands .= "/usr/bin/php5.6 {$config['normativka_portal_path']}/cli.php http://\${DOMAIN_NORMATIVKA}/services/deactivate/ --cwd=\${PORTAL_ROOT_PATH}". PHP_EOL;
@@ -32,7 +31,7 @@
 		$ordersIdsString = implode( ',', array_map( function ( $order ) use ( $pdo ) {
 			return $pdo->quote( $order['order_id'] );
 		}, $orders ) );
-		$stmt            = $pdo->query( "SELECT payment_id FROM nr_payments
+		$stmt = $pdo->query( "SELECT payment_id FROM nr_payments
 						 LEFT JOIN nr_invoices ON invoice_id = payment_invoice_id
 						 LEFT JOIN nr_orders ON order_id = invoice_order_id
 						 WHERE order_id IN ({$ordersIdsString}); " );
@@ -40,23 +39,20 @@
 		$payments = $stmt->fetchAll();
 
 		$paymentsIdsString = implode( ',', array_map( function ( $payment ) use ( $pdo ) {
-			return $pdo->quote( $payment['order_id'] );
+			return $pdo->quote( $payment['payment_id'] );
 		}, $payments ) );
-		$stmt              = $pdo->query( "DELETE FROM nr_payments WHERE payment_id IN ({$paymentsIdsString});" );
-		$stmt->execute();
-		$result[] = "Удалено оплат: {$stmt->rowCount()}";
+		$count = $pdo->exec("DELETE FROM nr_payments WHERE payment_id IN ({$paymentsIdsString});");
+		$result[] = "Удалено оплат: {$count}";
 
-		$stmt = $pdo->query( "DELETE FROM nr_invoices WHERE invoice_order_id IN ({$ordersIdsString});" );
-		$stmt->execute();
-		$result[] = "Удалено счетов: {$stmt->rowCount()}";
+		$count = $pdo->exec("DELETE FROM nr_invoices WHERE invoice_order_id IN ({$ordersIdsString});");
+		$result[] = "Удалено счетов: {$count}";
 
-		$stmt = $pdo->query( "DELETE FROM nr_codes WHERE code_order_id IN ({$ordersIdsString});" );
-		$stmt->execute();
-		$result[] = "Удалено кодов: {$stmt->rowCount()}";
+		$count = $pdo->exec("DELETE FROM nr_codes WHERE code_order_id IN ({$ordersIdsString});");
+		$result[] = "Удалено кодов: {$count}";
 
-		$stmt = $pdo->query( "DELETE FROM nr_orders WHERE order_id IN ({$ordersIdsString});" );
-		$stmt->execute();
-		$result[] = "Удалено заказов: {$stmt->rowCount()}";
+		$count = $pdo->exec("DELETE FROM nr_orders WHERE order_id IN ({$ordersIdsString});");
+		$result[] = "Удалено заказов: {$count}";
+		$pdo->exec("DELETE FROM nr_package_order WHERE po_or_id IN ({$ordersIdsString});");
 	}
 	echo implode(PHP_EOL, $result) . PHP_EOL;
 	exit(0);
