0af2dc7abf06bd1b533c934606e52f6f182d65d1
gutsout <eratut@mail.ru>
2016-11-28 22:03:48 +0300

    Завершение ApplicationController

---
+++ learning/gulp/gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const gulp = require('gulp');
+
+gulp.task('default', function () {
+    //return gulp.src('source/**/*.*')
+    //    .pipe(gulp.dest('dest'));
+        console.log(gulp.src('source/**/*.*'));
+});
+gt
+
+
---
+++ learning/gulp/t.js
@@ -0,0 +1,6 @@
+/**
+ * Created by gutsout on 28.11.16.
+ */
+gulp.task('', function() {
+
+});
