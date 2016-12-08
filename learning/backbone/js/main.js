f07fc426bd38c6e2935abd5e3cce047c9ee5f49d
gutsout <eratut@mail.ru>
2016-12-08 22:21:17 +0300

    Завершение ApplicationController

--- learning/backbone/js/main.js
+++ learning/backbone/js/main.js
@@ -1,24 +1,82 @@
-var Person = Backbone.Model.extend({
-    defaults : {
-        name : 'Matt',
-        age : 27,
-        job : 'Sys'
-    },
+(function () {
+    window.App = {
+        Models : {},
+        Views : {},
+        Collections : {},
+    };
 
-    validate : function (attrs) {
-        if (attrs.age < 0) {
-            return 'Wrong age'
+    App.Models.Person;
+    App.Views.Person;
+    App.Collections.People;
+
+    window.template = function (id) {
+        return _.template($('#' + id).html());
+    };
+
+    App.Models.Person = Backbone.Model.extend({
+        defaults : {
+            name : 'Matt',
+            age : 27,
+            job : 'Sys'
+        },
+
+        validate : function (attrs) {
+            if (attrs.age < 0) {
+                return 'Wrong age'
+            }
+        },
+
+        walk : function () {
+            return this.get('name') + ' is walking';
         }
-    },
+    });
 
-    walk : function () {
-        return this.get('name') + ' is walking';
-    }
-});
+    App.Collections.People = Backbone.Collection.extend({
+        model : App.Models.Person
+    });
 
-//var p1 = new Person();
+    App.Views.People = Backbone.View.extend({
+        tagName : 'ul',
+        render : function () {
+            console.log(this.collection);
+            this.collection.each(function (person) {
+                var personView = new App.Views.Person({model : person});
+                this.$el.append(personView.render().el);
+            }, this);
+            return this;
+        }
 
-var p2 = new Person({name: 'Vasya', age : 36, job : 'Driver'});
-p2.on('invalid', function (model, error) {
-    console.log(error);
-});
+    });
+
+
+    App.Views.Person= Backbone.View.extend({
+        tagName : 'li',
+        template : template('personTemplate'),
+        render : function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    var peopleCollection = new App.Collections.People([
+        {
+            name : 'Petya',
+            age : 22,
+            job : 'Manager'
+        },
+        {
+            name : 'Adolf',
+            age : 33,
+            job : 'Cleaner'
+        },
+        {
+            name : 'Chuchmambek',
+            job : 'Builder'
+        }
+    ]);
+
+    var peopleView = new App.Views.People({collection : peopleCollection});
+
+    $(document.body).append(peopleView.render().el);
+
+}());
