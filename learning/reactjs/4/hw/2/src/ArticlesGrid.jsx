4d0fa2f32a8a05e067ab158b9e4d78c0f3ca3b98
gutsout <eratut@mail.ru>
2016-12-25 20:53:22 +0300

    Завершение ApplicationController

--- learning/reactjs/4/hw/2/src/ArticlesGrid.jsx
+++ learning/reactjs/4/hw/2/src/ArticlesGrid.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import GridList from 'material-ui/GridList';
+import {GridList, GridTile} from 'material-ui/GridList';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IconButton from 'material-ui/IconButton';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
-class ArticlesGrid extends GridList {
-    constructo() {};
-    public render() {
-        return `asd`;
+class ArticlesGrid extends React.Component {
+
+    constructor() {
+        super();
+        this.styleList = {
+            width: 500,
+            height: 450,
+            overflowY: 'auto',
+          }
     }
+
+    render() {
+        return (
+            <MuiThemeProvider>
+                <GridList cols={2} cellHeight={200} padding={1} style={this.styleList} >
+                 {
+                     this.props.articles.map(function (article) {
+                         return (
+                                <GridTile
+                                    title={article.title}
+                                    key={article.id}
+                                    actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                                    actionPosition="left"
+                                >
+                                    <img src={`img/${article.img}`} alt={article.title} />
+                                </GridTile>
+                         )
+                     })
+                 }
+                </GridList>
+            </MuiThemeProvider>
+        );
+    }
+
 }
 
 export default ArticlesGrid;
--- learning/reactjs/4/hw/2/src/main.js
+++ learning/reactjs/4/hw/2/src/main.js
@@ -1,4 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
 //noinspection JSUnresolvedVariable
 import articles from '../data/articles.json';
 import ArticlesGrid from './ArticlesGrid.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
+ReactDOM.render(
+    <ArticlesGrid articles={articles} />,
+    document.getElementById('mount-point')
+);
