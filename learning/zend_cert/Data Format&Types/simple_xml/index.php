00c55a4c3d76245e942cf2cde93ca48dbe286b8d
gutsout <eratut@mail.ru>
2017-04-16 00:19:01 +0300

    zend xml

--- learning/zend_cert/Data Format&Types/simple_xml/index.php
+++ learning/zend_cert/Data Format&Types/simple_xml/index.php
@@ -1,9 +1,38 @@
 <?php
 
-$xmlFilePath = function (): string {return __DIR__ . '/../library.xml';};
+$xmlString = file_get_contents(__DIR__ . '/../library.xml');
+$xmlString = preg_replace('/(?:[\x00-\x1F\x7F]|[ ]{2,})/u', '', $xmlString);
+$library = simplexml_load_string($xmlString);
 
-$library = simplexml_load_file($xmlFilePath());
+if (php_sapi_name() === 'cli') {
+    $echo = function (string $string): void {echo $string . PHP_EOL;};
+} else {
+    $echo = function (string $string): void {echo $string . '<br />';};
+}
 
-$xmlStr = file_get_contents($xmlFilePath());
-$library = new SimpleXMLElement($xmlStr);
-$f = 1;
+$book = $library->addChild('book');
+$book->addAttribute('isbn', '978-5-17-100400-2');
+$book->addAttribute('title', 'Господа Головлевы');
+$book->addChild('author', 'Михаил Салтыков-Щедрин');
+$book->addChild('publisher', 'Neoclassic');
+
+/** @var $child SimpleXMLElement */
+/** @var $attribute SimpleXMLElement */
+/** @var $subChild SimpleXMLElement */
+
+
+/*foreach ($library->children() as $child) {
+    $echo($child->getName());
+
+    foreach ($child->attributes() as $attribute) {
+        $echo("{$attribute->getName()}: {$attribute}");
+    }
+
+    foreach ($child->children() as $subChild) {
+        $echo("{$subChild->getName()}: {$subChild}");
+    }
+    $echo(str_repeat('-', 50) . PHP_EOL);
+}*/
+
+header('Content-Type: text\xml');
+echo $library->asXML(/*__DIR__ . '/../library.xml'*/);
