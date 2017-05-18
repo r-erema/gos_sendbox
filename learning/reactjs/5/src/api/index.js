a1e597a0be88b238337950180125684ae7454092
gutsout <r.yaroma@corp.profigroup.by>
2017-05-18 14:20:17 +0300

    cfg

--- learning/reactjs/5/src/api/index.js
+++ learning/reactjs/5/src/api/index.js
@@ -3,35 +3,18 @@ const SCOPES = ['https://www.googleapis.com/auth/tasks', 'https://www.googleapis
 
 export default {
     authorize(params) {
-        //console.log(gapi.auth2.authorize);
         return new Promise((resolve, reject) => {
-            /*gapi.auth2.authorize(
+            gapi.auth.authorize(
                 {
-                    'client_id' : CLIENT_ID,
-                    'scope' : SCOPES,
-                    'immediate' : params.immediate,
-                    'cookie_policy' : 'single_host_origin'
+                    'client_id': CLIENT_ID,
+                    'scope': SCOPES,
+                    'immediate': params.immediate,
+                    'cookie_policy': 'single_host_origin'
                 },
                 authResult => {
-                    //console.log(authResult);
+                    console.log(authResult);
                 }
-            );*/
-            gapi.client.init({
-              //discoveryDocs: DISCOVERY_DOCS,
-              clientId: CLIENT_ID,
-              immediate : params.immediate,
-              scope: SCOPES
-            }).then(function () {
-              // Listen for sign-in state changes.
-              gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-              // Handle the initial sign-in state.
-              updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-              authorizeButton.onclick = handleAuthClick;
-              signoutButton.onclick = handleSignoutClick;
-            });
+            );
         });
-
-
-    }
+    },
 }
