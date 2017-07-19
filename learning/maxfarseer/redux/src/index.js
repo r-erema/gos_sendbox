e66dddcc6a4e702eb00c8c28b95fbfe4a8254c12
gutsout <eratut@mail.ru>
2017-07-19 21:59:38 +0300

    p-s

--- learning/maxfarseer/redux/src/index.js
+++ learning/maxfarseer/redux/src/index.js
@@ -1,9 +1,15 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import App from './container/App';
+import { Provider } from 'react-redux';
+import { configureStore } from './store/configureStore';
+import App from './containers/App';
+
+const store = configureStore();
 
 render(
-    <App />,
+    <Provider store={store} >
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
