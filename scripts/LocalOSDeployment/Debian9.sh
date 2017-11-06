2e1cb9cc907f52c70875a0de556022416c31a1aa
gutsout <eratut@mail.ru>
2017-11-06 16:16:37 -0500

    progress

--- scripts/LocalOSDeployment/Debian9.sh
+++ scripts/LocalOSDeployment/Debian9.sh
@@ -27,6 +27,9 @@ echo "force_color_prompt=yes" >> /root/.bashrc;
 #sudo
 apt-get install sudo -y;
 
+#htop
+apt-get install htop -y;
+
 #aptitude
 apt-get install aptitude -y;
 
@@ -52,6 +55,12 @@ wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg;
 sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list';
 apt update;
 apt install php7.1-common  php7.1-readline php7.1-fpm php7.1-cli php7.1-gd php7.1-mysql php7.1-mcrypt php7.1-curl php7.1-mbstring php7.1-opcache php7.1-json php7.1-xdebug -y;
+
+#Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
 
 #Git
 apt-get install git -y;
