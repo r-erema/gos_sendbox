7963e7a2d5813a14404886e69ed7610334a3fb55
gutsout <eratut@mail.ru>
2017-11-12 12:19:28 +0300

    fix

--- scripts/LocalOSDeployment/Debian9.sh
+++ scripts/LocalOSDeployment/Debian9.sh
@@ -68,6 +68,12 @@ echo 'xdebug.remote_host="localhost"' >> /etc/php/7.1/mods-available/xdebug.ini;
 echo 'xdebug.remote_port=9000' >> /etc/php/7.1/mods-available/xdebug.ini;
 echo 'xdebug.idekey="PHPSTORM"' >> /etc/php/7.1/mods-available/xdebug.ini;
 
+#MySql
+apt install mysql-server mysql-client -y;
+
+#phpmyadmin
+apt install phpmyadmin -y;
+
 #Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
