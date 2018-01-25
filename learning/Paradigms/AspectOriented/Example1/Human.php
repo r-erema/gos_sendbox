c462ef1d2b2add05d3ecba9430daa3e561d50483
gutsout <eratut@mail.ru>
2018-01-25 18:03:42 +0300

    AspectOriented beginning

---
+++ learning/Paradigms/AspectOriented/Example1/Human.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace learning\Paradigms\AspectOriented\Example1;
+
+
+class Human
+{
+
+    public function eat()
+    {
+        echo 'eating...', PHP_EOL;
+    }
+
+    public function cleanTeeth()
+    {
+        echo 'Cleaning teeth...', PHP_EOL;
+    }
+
+    public function washUp()
+    {
+        echo 'Washing up...', PHP_EOL;
+    }
+
+    public function work()
+    {
+        echo 'Working...', PHP_EOL;
+    }
+
+    public function sleep()
+    {
+        echo 'Go to sleep', PHP_EOL;
+    }
+
+}
---
+++ learning/Paradigms/AspectOriented/Example1/Tests/AspectOrientedTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace learning\Paradigms\AspectOriented\Example1\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AspectOrientedTest extends TestCase
+{
+    public function testHuman()
+    {
+        $this->expectOutputString('');
+    }
+}
