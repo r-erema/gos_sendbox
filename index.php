d7af422acd483627b45e07887957a70501523484
r.erema <roma.a.erema@gmail.com>
2018-10-11 17:51:48 +0300

    test commit

--- index.php
+++ index.php
@@ -9,3 +9,4 @@
 	<li><a href="phpinfo.php"><?php echo 'PHP '.phpversion(); ?></a></li>
 	<?php if (function_exists('apache_get_version')): ?><li><?php echo 'Apache '. apache_get_version(); ?></li><?php endif; ?>
 </ul>
+<?php /* test update */ ?>
