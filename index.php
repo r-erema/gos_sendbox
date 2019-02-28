74a1d7f093e11a7614dc6dd8e810514f4193deb9
r.erema <roma.a.erema@gmail.com>
2019-02-28 17:08:45 +0300

    Update

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     </head>
     <body>
-        <span></span>
-        <span></span>
-        <span></span>
         <ul>
-	        <li></li>
-	        <li></li>
-	        <li></li>
             <li><a href="phpinfo.php"><?php echo 'PHP '.phpversion(); ?></a></li>
             <?php if (function_exists('apache_get_version')): ?><li><?php echo 'Apache '. apache_get_version(); ?></li><?php endif; ?>
         </ul>
