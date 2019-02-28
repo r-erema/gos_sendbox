5d2b8b0f4628e29fc8372c05fddfc3f4bab295d2
r.erema <roma.a.erema@gmail.com>
2019-02-28 15:51:49 +0300

    index.html fix

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-</head>
-<body>
-<ul>
-	<li><a href="phpinfo.php"><?php echo 'PHP '.phpversion(); ?></a></li>
-	<?php if (function_exists('apache_get_version')): ?><li><?php echo 'Apache '. apache_get_version(); ?></li><?php endif; ?>
-</ul>
+    <head>
+        <meta charset="utf-8">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+        <ul>
+            <li><a href="phpinfo.php"><?php echo 'PHP '.phpversion(); ?></a></li>
+            <?php if (function_exists('apache_get_version')): ?><li><?php echo 'Apache '. apache_get_version(); ?></li><?php endif; ?>
+        </ul>
+    </body>
+</html>
