6dc77141343ee0ed3d5c3370e454bccbca271b2c
gutsout <eratut@mail.ru>
2019-03-03 21:17:13 +0300

    Multiton pattern

---
+++ learning/Patterns/Multiton/Example1/Multiton.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace learning\Patterns\Multiton\Example1;
+
+final class Multiton
+{
+
+    private static $instances = [];
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(string $instanceName): Multiton
+    {
+        if (!isset(self::$instances[$instanceName])) {
+            self::$instances[$instanceName] = new self();
+        }
+        return self::$instances[$instanceName];
+    }
+
+    private function __clone()
+    {}
+
+    private function __wakeup()
+    {}
+
+}
---
+++ learning/Patterns/Multiton/Example1/Tests/MultitonTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace learning\Patterns\Multiton\Example1\Tests;
+
+use learning\Patterns\Multiton\Example1\Multiton;
+use PHPUnit\Framework\TestCase;
+
+class MultitonTest extends TestCase
+{
+
+    public function testMultiton(): void
+    {
+        $instance1 = Multiton::getInstance(1);
+        $instance2 = Multiton::getInstance(2);
+
+        $this->assertNotSame($instance1, $instance2);
+        $this->assertSame($instance1, Multiton::getInstance(1));
+        $this->assertSame($instance2, Multiton::getInstance(2));
+    }
+}
