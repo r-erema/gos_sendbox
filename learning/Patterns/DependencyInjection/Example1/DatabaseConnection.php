055d5bdf6b713b5280877edf0a439f86b2816843
gutsout <eratut@mail.ru>
2019-03-08 13:13:52 +0300

    DI pattern

--- learning/Patterns/DependencyInjection/Example1/DatabaseConnection.php
+++ learning/Patterns/DependencyInjection/Example1/DatabaseConnection.php
@@ -5,23 +5,13 @@ namespace learning\Patterns\DependencyInjection\Example1;
 class DatabaseConnection
 {
 
-    /**
-     * @var DatabaseConfiguration
-     */
     private $configuration;
 
-    /**
-     * DatabaseConnection constructor.
-     * @param DatabaseConfiguration $configuration
-     */
     public function __construct(DatabaseConfiguration $configuration)
     {
         $this->configuration = $configuration;
     }
 
-    /**
-     * @return string
-     */
     public function getDsn(): string
     {
         return sprintf(
